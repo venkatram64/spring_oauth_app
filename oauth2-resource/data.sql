77c35d7c2f4b9b2d88f4483814d95a1816c3eef1
venkatram64 <venkat.veerareddy@hotmail.com>
2019-07-02 08:53:18 +0530

    init commit

---
+++ oauth2-resource/data.sql
@@ -0,0 +1,3 @@
+ INSERT INTO customr (first_name, last_name, email)
+ VALUES('venkatram','veerareddy','venkat.veerareddy@gmail.com'),
+ ('srijan','veerareddy','srijan.veerareddy@gmail.com');
---
+++ oauth2-resource/schema.sql
@@ -0,0 +1,11 @@
+
+SET sql_mode = '';
+
+
+create table if not exists  customer (
+  id int(11) not null auto_increment,
+  first_name varchar(50) default null,
+  last_name varchar(50) default null,
+  email varchar(150) default null
+  primary key (client_id)
+) engine=innodb;
